a6081edbaca5757f2de8f6c589d6916da7882b64
jayPhantomic <jayPhantomic@users.noreply.github.com>
2023-05-14 04:23:47 -0500

    lo

---
+++ public/index.search.js
@@ -0,0 +1,44 @@
+var relearn_search_index = [
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "First Content",
+    "uri": "/basics/first-content/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Second Content",
+    "uri": "/basics/second-content/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "TCX IO Site",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
